c5d1651f9c0f3b9c603b60374d7ee112bd8e1360
Nitin Rawat <91044967+Nitin-Rawat@users.noreply.github.com>
2024-06-16 01:55:05 +0530

    Completed the sales Dashboard Assignment

--- 09SalesDashbordProject/src/App.jsx
+++ 09SalesDashbordProject/src/App.jsx
@@ -1,66 +1,16 @@
-// import React from 'react'
-// import Orders from './components/Orders/Orders';
-// import Login from './components/Login/Login';
-// import { Outlet } from "react-router-dom";
-// import ThemeToggle from "./components/ThemeToggle/ThemeToggle";
-
-
-// function App() {
-//   const isAuthenticated = localStorage.getItem("auth") === "true";
-
-//   return (
-//     <>
-//       <ThemeToggle />
-//       <Login />
-//       {isAuthenticated && <Outlet />}
-//       {isAuthenticated && <Orders />}
-//     </>
-//   );
-// }
-
-
-// export default App
-
-
-// import * as React from "react";
-// import { Outlet, Navigate } from "react-router-dom";
-
-// function ProtectedRoute({ children }) {
-//   const isAuthenticated = localStorage.getItem("auth") === "true";
-
-//   return isAuthenticated ? children : <Navigate to="/" />;
-// }
-
-// function App() {
-//   return (
-//     <RouterProvider router={router}>
-//       <Routes>
-//         <Route path="/" element={<Login />} />
-//         <Route
-//           path="orders"
-//           element={
-//             <ProtectedRoute>
-//               <Orders />
-//             </ProtectedRoute>
-//           }
-//         />
-//       </Routes>
-//     </RouterProvider>
-//   );
-// }
-
-// export default App;
-
-// +++++++++++++++++++++++++++++++++🫡🫡🫡 Dont Change above code without saving below one 🥲🥲🥲+++++++++++++++++++++++++++++
 
 
 import React from "react";
 import { Outlet } from "react-router-dom";
 import ThemeToggle from "./components/ThemeToggle";
+import SaleOrderFormModal from "./components/Orders/SaleOrderFormModal"
+
+
 
 const App = () => {
   return (
     <div>
+      <SaleOrderFormModal/>
       <ThemeToggle />
       <Outlet />
     </div>
--- 09SalesDashbordProject/src/main.jsx
+++ 09SalesDashbordProject/src/main.jsx
@@ -43,15 +43,19 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider, Route, createRoutesFromElements } from 'react-router-dom';
 import Login from './components/Login/Login';
-import Orders from './components/Orders/Orders';
+import Orders from './components/Orders/Orders'
 import Theme from "./components/Theme/Theme";
 import ThemeToggle from "./components/ThemeToggle/ThemeToggle";
+import SaleOrderForm from "./components/Orders/SaleOrderForm"
+import SaleOrderFormModal from "./components/Orders/SaleOrderFormModal"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="" element={<Login />} />
       <Route path="orders" element={<Orders />} />
+      <Route path="SaleOrderForm" element={<SaleOrderForm />} />
+      <Route path="SaleOrderFormModal" element={<SaleOrderFormModal />} />
     </>
   )
 );
