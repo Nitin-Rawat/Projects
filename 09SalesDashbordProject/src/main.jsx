d7f8960dc398ad36bae1e556f45ec636aa84f8ea
Nitin Rawat <91044967+Nitin-Rawat@users.noreply.github.com>
2024-06-16 02:02:44 +0530

    Remove Unused Lines

--- 09SalesDashbordProject/src/main.jsx
+++ 09SalesDashbordProject/src/main.jsx
@@ -1,42 +1,4 @@
 
-// // import App from './App.jsx'
-// import './index.css'
-// import * as React from "react";
-// import { ChakraProvider } from "@chakra-ui/react";
-// import * as ReactDOM from "react-dom/client";
-// import Login from './components/Login/Login';
-// import Orders from './components/Orders/Orders';
-// import {  Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
-// import { DarkMode } from '@chakra-ui/react';
-// import Theme from "./components/Theme/Theme";
-// import { ColorModeScript } from "@chakra-ui/color-mode";
-
-
-
-// const router = createBrowserRouter(
-//   createRoutesFromElements(
-//     <>
-//       <Route path="" element={<Login />} />
-//       <Route path="Orders" element={<Orders />} />
-//       <Route path="Darkmode" element={<DarkMode />} />
-//     </>
-//   )
-// );
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//  <React.StrictMode>
-//     <ChakraProvider theme={Theme}>
-//       <ColorModeScript initialColorMode={Theme.config.initialColorMode} />
-//       {/* <App /> */}
-//     <RouterProvider router={router} />
-//     </ChakraProvider>
-//   </React.StrictMode>
-// );
-
-
-
-// +++++++++++++++++++++++++++++++++🫡🫡🫡 Dont Change above code without saving below one 🥲🥲🥲+++++++++++++++++++++++++++++
-
 import './index.css';
 import * as React from "react";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
